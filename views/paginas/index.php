fcb2222a79b9d96bb7335266da22b430c5c63ea9
Nacho RM <joseignaciorm13@gmail.com>
2019-09-23 15:27:47 +0200

    Continuación maquetación home. Pendiente añadir imágenes, menu bar, icon list.

--- views/paginas/index.php
+++ views/paginas/index.php
@@ -25,8 +25,8 @@
 
             <!-- ************* SEO-HOME SECTION **************** -->
             <!-- SEO-HOME SECTION - Block one -->
-  <section class="seo__home center-content">
-      <article class="l-container">
+  <section class="seo__home center-content l-container">
+      <article class="">
         <div class="l-section">
             <p class="seo__home--txt center-content m-b">Hablaremos un poco de teoría de los fundamentos en posicionamiento SEO y las diferencias entre       Diseño web y desarrollo web</p>
             <h1 class="seo__home--title center-content">
@@ -36,7 +36,7 @@
         </div>
 
         <div class="l-section seo__home--sub-title center-block">
-            <h2 class="seo-title icon-location">¿Qué es <span class="color-home-title-2">SEO</span> y cómo funciona?</h2>
+            <h2 class="seo-title">¿Qué es <span class="color-home-title-2">SEO</span> y cómo funciona?</h2>
             <span class="border__bottom center-block"></span>   
         </div>
 
@@ -62,9 +62,9 @@
   </section>
 
             <!-- SEO-HOME SECTION - Block two -->
-  <section class="seo__home__block-2 l-section center-content">
-    <article class="l-container">
-        <div class="seo__home__block-2--content">
+  <section class="seo__home__block-2 l-section center-content l-container">
+    <article class="">
+        <div class="seo__home__block-2--content l-section">
             <div class="seo__home__block-2--col-1">
                 <div class="seo__home__block-2--items">
                     <div class="seo__home__block-2--sub-title">
@@ -95,34 +95,38 @@
   </section>
 
             <!-- SEO-HOME SECTION - Block three -->
-<section class="seo__home--block-3 l-section center-content">
+<section class="seo__home__info l-section center-content l-container">
     <article>
-        <h3>Algúnos de los puntos importantes que debes tener en cuenta para contratar un consultor SEO </h3>
-        <ul class="info__consultor-seo">
-            <li class="info_consultor-seo--items left-content">Primero que haga una auditoría de tu web y te diga que se puede mejorar y por qué, y cuáles son los objetivos a alcanzar en tu negocio.</li>
-            <li class="info__consultor-seo--items left-content">Trabajos que haya realizado y la experiencia con otros clientes.</li>
-            <li class="info__consultor-seo--items left-content">Tiempos estimados para empezar a ver las mejoras.</li>
-            <li class="info__consultor-seo--items left-content">Demostrar que se interesa en tu empresa.</li>
-            <li class="info__consultor-seo--items left-content">Que use buenas prácticas y sigue los requerimientos oficiales de SEO.</li>
+        <h3 class="seo__home__info--title">Algúnos de los puntos importantes que debes tener en cuenta para contratar un consultor SEO </h3>
+        <ul class="seo__home__info--consultor">
+            <li class="seo__home__info--item left-content icon-check">Primero que haga una auditoría de tu web y te diga que se puede mejorar y por qué, y cuáles son los objetivos a alcanzar en tu negocio.</li>
+            <li class="seo__home__info--item left-content icon-check">Trabajos que haya realizado y la experiencia con otros clientes.</li>
+            <li class="seo__home__info--item left-content icon-check">Tiempos estimados para empezar a ver las mejoras.</li>
+            <li class="seo__home__info--item left-content icon-check">Demostrar que se interesa en tu empresa.</li>
+            <li class="seo__home__info--item left-content icon-check">Que use buenas prácticas y sigue los requerimientos oficiales de SEO.</li>
         </ul>
-
-        <p class="small">Artículo de ayuda de google para contratar a un <a class="seo__home--link-in" href="https://support.google.com/webmasters/answer/35291" target="_blank">experto SEO</a></p>
-        <p class="small">Si necesitas saber más sobre posicionamiento seo, te dejamos una  <a class="seo__home--link-in" href="https://static.googleusercontent.com/media/www.google.es/es/es/webmasters/docs/guia_optimizacion_motores_busqueda.pdf" target="_blank">“Guía de optimización en buscadores (SEO)”</a> oficial de google donde encontrarás toda la documentación necesaria para mejorar tu contenido web.</p>
+        <div class="seo__home__info--action m-top-sect">
+            <p class="small">Artículo de ayuda de google para contratar a un <a class="seo__home--link-in" href="https://support.google.com/    webmasters/answer/35291" target="_blank">experto SEO</a></p>
+            <p class="small">Si necesitas saber más sobre posicionamiento seo, te dejamos una  <a class="seo__home--link-in" href="https:// static.googleusercontent.com/media/www.google.es/es/es/webmasters/docs/guia_optimizacion_motores_busqueda.pdf" target="_blank">“Guía de  optimización en buscadores (SEO)”</a> oficial de google donde encontrarás toda la documentación necesaria para mejorar tu contenido web.</   p>
+        </div>
     </article>
 </section>
 
-            <!-- SEO-HOME SECTION - Block four -->
-<section class="seo__home--block-4 center-content">
-    <article class="seo__home--article-block-4">
+            <!-- SEO-HOME SECTION Info 2 - Block four -->
+<section class="seo__home--info-2 center-content l-section m-top-sect l-container">
+    <article class="seo__home--info-2--col1">
         <h3>En Chips Designs nos adaptamos a tus necesidades y juntos podemos hacer que no sólo tus amigos o familiares sean los que visiten a tu página web</h3>
-        <p class="left-content">En Chips Designs nos enfocamos en el posicionamiento de tu página web, marca, negocio  o  tienda on-line para que no solo la conozcan tus amigos y familiares. Con trabajo, tiempo y dedicación de nosotros más la colaboración de nuestro cliente para poder adaptarnos a sus necesidades, lo convertiremos en una realidad cumpliendo con los objetivos que previamente hayamos aprobado en una propuesta de negocio.</p>
+        <p class="left-content">Nos enfocamos en el posicionamiento de tu página web, marca, negocio  o  tienda on-line para que no solo la conozcan tus amigos y familiares. Con trabajo, tiempo y dedicación de nosotros más la colaboración de nuestro cliente para poder adaptarnos a sus necesidades, lo convertiremos en una realidad cumpliendo con los objetivos que previamente hayamos aprobado en una propuesta de negocio.</p>
         <p class="left-content">Organizamos la información, estructuramos el contenido para que sea flexible y fácil de entender por los usuarios que visiten tu sitio. Trabajamos la sencillez evitando contenidos duplicados o de poco valor, es decir, buscamos la excelente experiencia de usuario (UX), para que los que entren en tu página web se puedan sentir a gusto gracias a la agradable visibilidad y usabilidad que percibirán gracias al excelente trabajo realizado. Obteniendo además un buen rendimiento de carga de la web
         favoreciendo el acceso rápido desde cualquier dispositivo móvil.
         </p>
-        <a href="posicionamiento-seo" class="buttom-1">Empecemos</a>
+        <div class="l-section">
+            <a href="posicionamiento-seo" class="buttom-1 btn-info-2">Empecemos</a>
+        </div>
 
     </article>
-    <article class="center-content">
+
+    <article class="seo__home--info-2--col2 center-content">
         <ul class="info__chipsDesigns center-content">
             <li class="info_chipsDesigns--items left-content">Conoce el proceso de trabajo de <a class="seo__home--link-in" href="posicionamiento-seo" target="_blank">posicionamiento web</a> que ofrecemos.</li>
             <li class="info__chipsDesigns--items left-content">Hacemos una auditoría 100% gratuita de tu página web y te hacemos una valoración sin compromisos. </li>
@@ -134,28 +138,41 @@
 
 
             <!-- ************* Diseño y desarrollo-HOME SECTION **************** -->
-<section class="web__home--block-1 l-section center-content">
-    <h1 class="web__home--title center-content m-top-sect">Diseño y desarrollo de páginas web</h1>
-    <h2>En que se diferencia el Diseño web con el Desarrollo web:</h2>
-    <p class="left-content">Diseño web en la actualidad engloba diseño y desarrollo web. Esto significa que ambas áreas están totalmente relacionadas. Un sitio web requiere de un buen diseño de páginas para lograr ese aspecto visual atractivo a traves de la optimización de imágenes, creación de banners, logotipos, iconos, entre otros..., un logotipo que transmita la identidad de tu empresa, para hacer más agradable y flexible la página web cautivando a los clientes gracias a ese trabajo realizado por el diseñador, Mientras que el desarrollador web se encarga de realizar la maquetación de la página, esa parte funcional que está detrás y que el usuario no lo ve pero es lo que la hace visible y dinámica. El desarrollador web dependiendo las necesidades y requerimientos del negocio, puede trabajar con herramientas de CMSs como Wordpress, Prestashop por nombrar algunas de ellas, usar Frameworks como Bootstrap, Foundation para el Front-end y Laravel, Python para el Back-end y  HTML5, CSS3, Java Script, PHP para poder lograr lo que es una creación de página web a medida.</p>
-    <p class="left-content">Estos son algunas características fundamentales entre las diferencias de un diseñador de páginas web y un desarrollador de páginas web. Debemos tener claro y ser conscientes como empresa de que un sitio web requiere un trabajo  en equipo entre diseñadores y desarrolladores para  poder lograr los objetivos y efectividad.</p>
-</section>
+<section class="web__home--block-1 l-section l-container center-content">
+    <h1 class="m-top-sect">Diseño y desarrollo de páginas web</h1>
+    <div class="web__home--items">
+        <article class="">
+            <div class="m-b">
+                <h2>¿Qué diferencias podemos encontrar entre el Diseño web y el Desarrollo web?</h2>
+                <span class="border__bottom center-block"></span>
+            </div>
+            <p class="left-content">Diseño web en la actualidad engloba diseño y desarrollo web. Esto significa que ambas áreas están totalmente        relacionadas. Un sitio web requiere de un buen diseño de páginas para lograr ese aspecto visual atractivo a traves de la optimización de     imágenes, creación de banners, logotipos, iconos, entre otros..., un logotipo que transmita la identidad de tu empresa, para hacer más        agradable y flexible la página web cautivando a los clientes gracias a ese trabajo realizado por el diseñador, Mientras que el     desarrollador   web se encarga de realizar la maquetación de la página, esa parte funcional que está detrás y que el usuario no lo ve   pero es lo que la hace  visible y dinámica. El desarrollador web dependiendo las necesidades y requerimientos del negocio, puede trabajar     con herramientas de CMSs   como Wordpress, Prestashop por nombrar algunas de ellas, usar Frameworks como Bootstrap, Foundation para el  Front-end y Laravel, Python para  el Back-end y  HTML5, CSS3, Java Script, PHP para poder lograr lo que es una creación de página web a  medida.</p>
+            <p class="left-content">Estos son algunas características fundamentales entre las diferencias de un diseñador de páginas web y un       desarrollador de páginas web. Debemos tener claro y ser conscientes como empresa de que un sitio web requiere un trabajo  en equipo entre       diseñadores y desarrolladores para  poder lograr los objetivos y efectividad.</p>
+        </article>
 
-<section class="web__home--block-2 l-section center-content">
-    <h2>¿Qué métodos empleamos en chips designs para el diseño web?</h2>
-    <ul class="list__web-method left-content">
-        <li class="list__web-method--items">Reunión en persona, correo electrónico o video llamada para iniciar las ideas y objetivos de la empresa.</li>
-        <li class="list__web-method--items">Presentación de propuesta siguiendo las ideas iniciales.</li>
-        <li class="list__web-method--items">Hacemos una arquitectura de la información.</li>
-        <li class="list__web-method--items">Hacemos una arquitectura web y estructuración del contenido.</li>
-        <li class="list__web-method--items">Optimización de la página con los estándares de posicionamiento web - SEO.</li>
-        <li class="list__web-method--items">Maquetación web adaptada a móviles, tablets, portátil y ordenadores de escritorio.</li>
-        <li class="list__web-method--items">Presentación del diseño.</li>
-        <li class="list__web-method--items">Feedback del cliente para últimos retoques de acuerdo a la propuesta aprobada previamente.</li>
-        <li class="list__web-method--items">Acabado de diseño y contenido.</li>
-        <li class="list__web-method--items">Fin y entrega del proyecto.</li>
-    </ul>
-    <p>Conoce nuestro flujo de trabajo con más detalle entrando a nuestra sección de <a href="diseño-web" class="buttom-1">diseño web.</a></p>
+        <article class="l-section">
+            <div class="m-b">
+                <h2>¿Qué métodos empleamos en chips designs para el diseño web?</h2>
+                <span class="border__bottom center-block"></span>
+            </div>
+            <ul class="list__web-method left-content m-b">
+                <li class="list__web-method--items icon-check">Reunión en persona, correo electrónico o video llamada para iniciar las ideas y  objetivos de la     empresa.</li>
+                <li class="list__web-method--items  icon-check">Presentación de propuesta siguiendo las ideas iniciales.</li>
+                <li class="list__web-method--items  icon-check">Hacemos una arquitectura de la información.</li>
+                <li class="list__web-method--items  icon-check">Hacemos una arquitectura web y estructuración del contenido.</li>
+                <li class="list__web-method--items  icon-check">Optimización de la página con los estándares de posicionamiento web - SEO.</li>
+                <li class="list__web-method--items  icon-check">Maquetación web adaptada a móviles, tablets, portátil y ordenadores de escritorio.</    li>
+                <li class="list__web-method--items  icon-check">Presentación del diseño.</li>
+                <li class="list__web-method--items  icon-check">Feedback del cliente para últimos retoques de acuerdo a la propuesta aprobada   previamente.</li>
+                <li class="list__web-method--items  icon-check">Acabado de diseño y contenido.</li>
+                <li class="list__web-method--items  icon-check">Fin y entrega del proyecto.</li>
+            </ul>
+            <div class="">
+                <p>Conoce nuestro flujo de trabajo con más detalle entrando a nuestra sección de <a href="diseño-web" class="buttom-1       btn-web-action">diseño web.</a></p>
+            </div>
+        </article>
+    </div>
+    
 </section>
             <!-- ************* Diseño y desarrollo-HOME SECTION **************** -->
 
