8542bebdaf0b548faf0e6dd55673578c3fab15d4
Nacho RM <joseignaciorm13@gmail.com>
2019-10-02 16:57:37 +0200

    Continuación de sección tres de página consultoria SEO. Pendiente finalizar maquetación.

--- views/paginas/consultor-seo.php
+++ views/paginas/consultor-seo.php
@@ -11,11 +11,13 @@
 <main class="main__seo-container l-container">
 
     <!-- Banner top -->
-    <section class="banner__top">
+    <section class="banner__top border-b-sec-color">
         <div class="banner__top--description">
             <h1 class="banner__top--title">Trabajemos juntos y hagamos un excelente posicionamiento <br>web-seo</h1>
             <p class="banner__top--txt">Siguiendo las directrices de google de posicionamiento seo para generar tráfico de valor, aumentar los beneficios de tu negocio y que los usuarios puedan encontrar tu sitio web en las primeras posiciones de búsqueda orgánica.</p>
-            <div class="buttom-1 btnBannerTop center-block">Solicita una auditoria gratis</div>
+            <div class="buttonBox btnBannerTop">
+               <a href="#" class="buttom-1">Solicita una auditoria gratis</a> 
+            </div>
         </div>
         <div class="banner__top--image">
             <img src="assets/img/seo-banner.jpeg" alt="Banner posicionamiento SEO">
@@ -25,19 +27,179 @@
     <!-- End banner top -->
 
     <!-- Section 2 -->
-    <section class="seo__service__sec_2 l-section">
-        <div class="item-1 center-block center-content">
-            <h1 class="seo__service__sec_2--title">Si quieres mejorar tu contenido y optimización web-seo, empecemos a trabajar juntos para lograr dichos objetivos</h1>
-        </div>
+    <section class="seo__service__sec_2 l-section border-b-sec-color">
+        <!--<div class="item-1 center-block center-content">
+            <h1 class="seo__service__sec_2--title">Si quieres mejorar tu contenido y optimización web-seo, empecemos a trabajar para lograr dichos objetivos</h1>
+        </div>-->
         <div class="item-2">
-            <p class="m-b-txt">Somos una agencia SEO que elabora experiencias en línea, hermosas y  atractivas.</p>
-            <p class="m-b-txt">Trabajamos el performance de tu web, lo que favorece positivamente la experiencia de usuario que es necesaria para aumentar el tráfico de tu web.</p>
+            <h1 class="seo__service__sec_2--title">Si quieres mejorar tu contenido y optimización web-seo, empecemos a trabajar para lograr dichos objetivos</h1>
+            <div class="seo__service__sec_2--txt">
+                <p class="m-b-txt">Somos una agencia SEO que elabora experiencias en línea, hermosas y  atractivas.</p>
+                <p class="m-b-txt">Trabajamos el performance de tu web, lo que favorece positivamente la experiencia de usuario que es necesaria para aumentar el tráfico de tu web.</p>
+            </div>
+            <div class="buttonBox btnSeoServiceTop">
+                <a href="#" class="buttom-1">Solicita una auditoria gratis</a>
+            </div>
         </div>
         <div class="item-3 center-content">
             <img class="item-3--image" src="assets/img/Portatil-with-calendar.jpeg" alt="Servicios consultoría SEO">
         </div>
     </section>    
     <!-- End section 2 -->
+
+    <!-- Section 3 -->
+    <section class="seo__service__sec_3 l-section border-b-sec-color">
+        <div class="content-1 center-content">
+            <h2 class="content-1--title m-b">Técnicas que seguimos para Posicionamiento web</h2>
+            <p class="content-1--txt m-b">A continuación hemos seleccionado una pequeña lista relevante de las técnicas empleadas en nuestro flujo de trabajo <br>de <span class="txt-enfasis">Optimización SEO On Page y Off Page.<span></p>
+        </div>
+
+        <div class="content-2">
+            <div class="content-2--items">
+                <div class="titleBox">
+                    <h3 class="title">Código de respuesta</h3>
+                    <span class="border__bottom center-block l-section"></span>
+                </div>
+                <div class="txtBox">
+                    <p class="txt">Análisis exhaustivo de los enlaces web para reducir en lo posible redireccionamientos incorrectos, así como  enlaces rotos de páginas indexadas por google pero que ya no existen en el sitio web o páginas que han sido creadas y eliminadas     por usuarios y que estas ya no existen.
+                    </p>
+                </div>
+                <div class="buttonBox buttom center-block">
+                    <a href="#" class="buttomLink buttom-1">Auditoría gratis!</a>
+                </div>
+            </div>
+
+            <div class="content-2--items">
+                <div class="titleBox">
+                    <h3 class="title">URLs SEO Friendly</h3>
+                    <span class="border__bottom center-block l-section"></span>
+                </div>
+                <div class="txtBox">
+                    <p class="txt">Expresar mediante la url una relación con el contenido. Esto ayuda a que google entienda la página web.
+                    </p>
+                </div>
+                <div class="buttom buttonBox center-block">
+                    <a href="#" class="buttomLink buttom-1">Auditoría gratis!</a>
+                </div>
+            </div>
+
+            <div class="content-2--items">
+                <div class="titleBox">
+                    <h3 class="title">Etiquetado HTML y metadatos</h3>
+                    <span class="border__bottom center-block l-section"></span>
+                </div>
+                <div class="txtBox">
+                    <p class="txt">Trabajamos rigurosamente el etiquetado y metadatos siguiendo las buenas prácticas, incluyendo las Keywords de interés para la captación de tráfico. Así evitamos dolores de cabeza y mejoras del ranking.
+                    </p>
+                </div>
+                <div class="buttom buttonBox center-block">
+                    <a href="#" class="buttomLink buttom-1">Auditoría gratis!</a>
+                </div>
+            </div>
+
+            <div class="content-2--items">
+                <div class="titleBox">
+                    <h3 class="title">Control de los enlaces salientes (internos y externos)</h3>
+                    <span class="border__bottom center-block l-section"></span>
+                </div>
+                <div class="txtBox">
+                    <p class="txt">Buscamos la fluidez de navegación, que se sienta la armonía y naturalidad controlando el Page Rank.
+                    </p>
+                </div>
+                <div class="buttom buttonBox center-block">
+                    <a href="#" class="buttomLink buttom-1">Auditoría gratis!</a>
+                </div>
+            </div>
+
+            <div class="content-2--items">
+                <div class="titleBox">
+                    <h3 class="title">Canibalizaciones</h3>
+                    <span class="border__bottom center-block l-section"></span>
+                </div>
+                <div class="txtBox">
+                    <p class="txt">Análisis riguroso para evitar la competencia entre páginas internas favoreciendo el tráfico y crowdbuyer al contenido principal o mejor dicho al contenido que queremos posicionar.
+                    </p>
+                </div>
+                <div class="buttom buttonBox center-block">
+                    <a href="#" class="buttomLink buttom-1">Auditoría gratis!</a>
+                </div>
+            </div>
+
+            <div class="content-2--items">
+                <div class="titleBox">
+                    <h3 class="title">Performance</h3>
+                    <span class="border__bottom center-block l-section"></span>
+                </div>
+                <div class="txtBox">
+                    <p class="txt">Mejorar el performance de un sitio web es sumamente importante para evitar un consumo innecesario de los recursos del servidor.
+                    </p>
+                </div>
+                <div class="buttom buttonBox center-block">
+                    <a href="#" class="buttomLink buttom-1">Auditoría gratis!</a>
+                </div>
+            </div>
+
+            <div class="content-2--items">
+                <div class="titleBox">
+                    <h3 class="title">Link Building (construcción de enlaces)</h3>
+                    <span class="border__bottom center-block l-section"></span>
+                </div>
+                <div class="txtBox">
+                    <p class="txt">Necesario para posicionar, además debemos tener un buen contenido de valor para que nos puedan enlazar. Nuestra estrategia es elaborar un proceso previo de planificación que ejecutaremos con el fín de conseguir que enlacen a nuestro proyecto desde los mejores sitios y lograr incrementar nuestra autoridad.
+                    </p>
+                </div>
+                <div class="buttom buttonBox center-block">
+                    <a href="#" class="buttomLink buttom-1">Auditoría gratis!</a>
+                </div>
+            </div>
+
+            <div class="content-2--items">
+                <div class="titleBox">
+                    <h3 class="title">Mobile-first indexing</h3>
+                    <span class="border__bottom center-block l-section"></span>
+                </div>
+                <div class="txtBox">
+                    <p class="txt">Hoy en día el tráfico web de móviles es mayor que el de escritorio. De acuerdo a la documentación oficial     
+                     <a href="https://developers.google.com/search/mobile-sites/mobile-first-indexing?hl=es-419" target="_blank" rel="noopener noreferrer"></a>, google rastrea e indexa primeramente la versión móvil que la de escritorio, por lo tanto, la versión móvil debe tener un contenido funcional para poder clasificar. 
+                    </p>
+                </div>
+                <div class="buttom buttonBox center-block">
+                    <a href="#" class="buttomLink buttom-1">Auditoría gratis!</a>
+                </div>
+            </div>
+
+            <div class="content-2--items">
+                <div class="titleBox">
+                    <h3 class="title">SEO local</h3>
+                    <span class="border__bottom center-block l-section"></span>
+                </div>
+                <div class="txtBox">
+                    <p class="txt">Factor importante para un negocio con espacio físico que necesita generar tráfico de usuarios de la misma zona. Además que es un punto a favor tanto para los consumidores en concreto como para google.
+                    Empleamos estratégias y técnicas necesarias para implementar el SEO local, por supuesto, aplicando buenas prácticas On-Site y Off-Page.
+                    </p>
+                </div>
+                <div class="buttom buttonBox center-block">
+                    <a href="#" class="buttomLink buttom-1">Auditoría gratis!</a>
+                </div>
+            </div>
+        </div>
+    </section>
+    <!-- End section 3 -->
+
+    <!-- Section 4 -->
+
+
+    <!-- End section 4 -->
+
+    <!-- Section 5 -->
+
+
+    <!-- End section 5 -->
+
+    <!-- Section 6 -->
+
+
+    <!-- End section 6 -->
 
     
 </main>
